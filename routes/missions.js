fa3afb959ab7a881551f416da84c7436d82a6141
Jesue Junior <jesuesousa@gmail.com>
2014-04-13 02:15:46 -0300

    Routes to mission earth

--- routes/missions.js
+++ routes/missions.js
@@ -1,11 +1,9 @@
 module.exports = function(app){
-    app.get('/', function (req, res) {
-        res.render('webapp/prova/fogo');
-    });
-};
-
-module.exports = function(app){
     app.get('/prova/fogo', function(req,res) {
         res.render('webapp/prova/fogo')
+    });
+
+    app.get('/mission/earth', function(req, res){
+        res.render('webapp/prova/terra')
     });
 };
