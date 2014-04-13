fe6d589fc3c5efb6481deadf136e891334554006
Ciro S. Costa <ciro.costa@usp.br>
2014-04-13 12:14:01 -0300

    updates fire page

--- public/assets/scripts/fire.js
+++ public/assets/scripts/fire.js
@@ -9,6 +9,7 @@
     var map;
     var mc;
     var markers = [];
+    var currentMarker;
 
     function insertMarkersFromData (data, map) {
         for (var index in data) {
@@ -21,7 +22,6 @@
     }
 
     function addMarker (map, position, notMove) {
-        console.log(position);
         var marker = new google.maps.Marker({
             position: position,
             animation: google.maps.Animation.DROP,
@@ -65,15 +65,15 @@
 
     function initialize() {
         var mapOptions = {
-            center: new google.maps.LatLng(-34.397, 150.644),
+            center: new google.maps.LatLng(28.3043, 29.179),
+            scrollwheel: false,
             zoom: 2,
-            style: [{"featureType":"poi","stylers":[{"visibility":"off"}]},{"stylers":[{"saturation":-70},{"lightness":37},{"gamma":1.15}]},{"elementType":"labels","stylers":[{"gamma":0.26},{"visibility":"off"}]},{"featureType":"road","stylers":[{"lightness":0},{"saturation":0},{"hue":"#ffffff"},{"gamma":0}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"lightness":50},{"saturation":0},{"hue":"#ffffff"}]},{"featureType":"administrative.province","stylers":[{"visibility":"on"},{"lightness":-50}]},{"featureType":"administrative.province","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"labels.text","stylers":[{"lightness":20}]}]
+            styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]}]
         };
 
-        map = new google.maps.Map(document.getElementById("map"),
-                                      mapOptions);
+        map = new google.maps.Map(document.getElementById("map"), mapOptions);
         geocoder = new google.maps.Geocoder();
-        addMapClickListener(map);
+        // addMapClickListener(map);
         insertMarkersFromData(geoData, map);
         mc = new MarkerClusterer(map, markers);
     }
@@ -89,7 +89,12 @@
 
             var inputVal = $('.map-input').val();
             codeAddress(geocoder, inputVal, function (latLng) {
-                addMarker(map, latLng);
+
+                if (currentMarker) {
+                    currentMarker.setMap(null);
+                }
+
+                currentMarker = addMarker(map, latLng);
             });
         });
     });
